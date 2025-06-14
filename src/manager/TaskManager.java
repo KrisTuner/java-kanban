c5b2c2a979541df9d0a7b7b41acae1d6c453617f
Lisendor <Lisendor@mail.ru>
2025-06-14 21:02:03 +0700

    Спринт 6, а так же исправление для тестов на Github

--- src/manager/TaskManager.java
+++ src/manager/TaskManager.java
@@ -6,22 +6,31 @@ import model.Task;
 import java.util.List;
 
 public interface TaskManager {
+
     List<Task> getAllTasks();
+
     List<Epic> getAllEpics();
+
     List<Subtask> getAllSubtasks();
 
     void createTask(Task task);
+
     void createEpic(Epic epic);
+
     void createSubtask(Subtask subtask);
 
     void updateTask(Task task);
+
     void updateSubtask(Subtask subtask);
 
     Task getTask(int id);
+
     Epic getEpic(int id);
+
     Subtask getSubtask(int id);
 
     void deleteTaskById(int id);
+
     void deleteEpicById(int id);
 
     List<Task> getHistory();
